007d58bad161c4640bdc10060933ab172d73b055
stardvst <lilia.manukyan@hotmail.com>
2022-11-30 22:37:40 +0400

    getting posts with scully

--- 2-blog-app-scully-router/src/app/articles/articles.component.ts
+++ 2-blog-app-scully-router/src/app/articles/articles.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
+  posts: ScullyRoute[] = [];
+  private _routeSub: Subscription | undefined;
 
-  constructor() { }
+  constructor(private _scullyService: ScullyRoutesService) {}
 
   ngOnInit(): void {
+    this._routeSub = this._scullyService.available$.subscribe(posts => {
+      this.posts = posts.filter(post => post.title);
+    });
   }
 
+  ngOnDestroy(): void {
+    this._routeSub?.unsubscribe();
+  }
 }
