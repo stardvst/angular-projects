7b5dd8e4a5670a65effa093e527cf28b2fed5a05
stardvst <lilia.manukyan@hotmail.com>
2022-12-04 20:29:39 +0400

    added new issue reporting form

--- 3-issue-tracker-reactive-forms/src/app/issue-report/issue-report.component.ts
+++ 3-issue-tracker-reactive-forms/src/app/issue-report/issue-report.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-report',
   templateUrl: './issue-report.component.html',
-  styleUrls: ['./issue-report.component.css']
+  styleUrls: ['./issue-report.component.css'],
 })
 export class IssueReportComponent implements OnInit {
+  issueForm: FormGroup | undefined;
 
-  constructor() { }
+  constructor(private builder: FormBuilder, private issueService: IssuesService) {}
 
   ngOnInit(): void {
+    this.issueForm = this.builder.group({
+      title: [''],
+      description: [''],
+      priority: [''],
+      type: [''],
+    });
   }
 
+  addIssue() {
+    this.issueService.createIssue(this.issueForm?.value);
+  }
 }
