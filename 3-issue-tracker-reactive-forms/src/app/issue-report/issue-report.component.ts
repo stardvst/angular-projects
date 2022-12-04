d34886d543d7ae332dbbc3b630e42f46ea85f5cd
stardvst <lilia.manukyan@hotmail.com>
2022-12-04 20:22:38 +0400

    added issue report component

---
+++ 3-issue-tracker-reactive-forms/src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.css']
+})
+export class IssueReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
