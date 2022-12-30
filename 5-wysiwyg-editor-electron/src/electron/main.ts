5a2ddc0252642724ff629e419c716c3590334e97
stardvst <lilia.manukyan@hotmail.com>
2022-12-30 22:15:40 +0400

    configured angular cli workspace for communication between apps

--- 5-wysiwyg-editor-electron/src/electron/main.ts
+++ 5-wysiwyg-editor-electron/src/electron/main.ts
@@ -4,6 +4,10 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
   mainWindow.loadFile('index.html');
 }
