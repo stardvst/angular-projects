78ba909f962f40175d543fa41da08d9fb0bcc0bc
stardvst <lilia.manukyan@hotmail.com>
2023-01-15 17:26:52 +0400

    adding title, meta tags to app

--- 7-github-portfolio/src/app/app.component.ts
+++ 7-github-portfolio/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = '7-github-portfolio';
+export class AppComponent implements OnInit {
   username = environment.username;
+
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Github Portfolio App');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s Github portfolio`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
+  }
 }
