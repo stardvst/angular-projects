71caf434e25e25784e225337f1176ef32856fa98
stardvst <lilia.manukyan@hotmail.com>
2023-01-15 17:40:57 +0400

    added preboot lib to queue events during app loading

--- 7-github-portfolio/src/app/app.module.ts
+++ 7-github-portfolio/src/app/app.module.ts
@@ -8,10 +8,16 @@ import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { PrebootModule } from 'preboot';
 
 @NgModule({
   declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent, OrganizationsComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule, TransferHttpCacheModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    TransferHttpCacheModule,
+    PrebootModule.withConfig({ appRoot: 'app-root' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
