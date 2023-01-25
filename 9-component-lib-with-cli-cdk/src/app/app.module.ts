bdf3d756666139cf89b7a7cb36e22ef0ac22d151
stardvst <lilia.manukyan@hotmail.com>
2023-01-25 21:56:58 +0400

    interaction with clipboard

--- 9-component-lib-with-cli-cdk/src/app/app.module.ts
+++ 9-component-lib-with-cli-cdk/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UiControlsModule } from 'ui-controls';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    UiControlsModule
+  BrowserModule,
+    UiControlsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
